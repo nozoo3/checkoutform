bfa6635beeceb83cdf0b03ffc074dc07c9965ff9
nozomi <dog1518rocky@gmail.com>
2025-02-24 18:05:24 +0900

    feat: Dispaly payment method and address

--- src/components/features/CheckoutForm/ConfirmModal.tsx
+++ src/components/features/CheckoutForm/ConfirmModal.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal';
+import { useFormContext } from 'react-hook-form';
+import { FormField } from '.';
 import { Heading2 } from '~/components/ui/Heading';
+import { formatPhoneNumber, formatPostCode } from './utils';
 
 type Props = {
   isOpen: boolean;
@@ -7,7 +10,22 @@ type Props = {
   handleConfirm: () => void;
 };
 
+const PAYMENT_METHODS = {
+  credit: 'クレジットカード(Visa, MasterCard, JCB, American Express)',
+  carrierPayment: 'キャリア決済',
+  convenienceStore: 'コンビニ決済',
+} as const;
+
 export const ConfirmModal = ({ isOpen, handleClose }: Props) => {
+  const methods = useFormContext<FormField>();
+  const { getValues } = methods;
+
+  const paymentMethodKey = getValues('paymentMethod');
+  const paymentMethodValue =
+    PAYMENT_METHODS[paymentMethodKey as keyof typeof PAYMENT_METHODS];
+  const fullName = `${getValues('firstName')} ${getValues('lastName')} 様`;
+  const postCode = `〒${formatPostCode(String(getValues('postCode')))}`;
+
   const customStyles = {
     content: {
       top: '50%',
@@ -19,6 +37,7 @@ export const ConfirmModal = ({ isOpen, handleClose }: Props) => {
       minWidth: '70%',
     },
   };
+
   return (
     <Modal style={customStyles} isOpen={isOpen} onRequestClose={handleClose}>
       <div className="mb-4">
@@ -30,9 +49,27 @@ export const ConfirmModal = ({ isOpen, handleClose }: Props) => {
         </div>
         <div className="w-full border border-gray-300"></div>
       </div>
-      <div className="p-4">
-        <Heading2>お支払い方法</Heading2>
-        <Heading2>配送先</Heading2>
+      <div className="p-8">
+        <div className="mb-4">
+          <Heading2>お支払い方法</Heading2>
+        </div>
+        <div className="mb-4 p-2 outline outline-gray-300">
+          <p>{paymentMethodValue}</p>
+        </div>
+        <div className="mb-4">
+          <Heading2>配送先</Heading2>
+        </div>
+        <div className="p-2 outline outline-gray-300">
+          <p className="font-bold">{fullName}</p>
+          <p>{formatPhoneNumber(String(getValues('phone')))}</p>
+          <p>{postCode}</p>
+          <div className="flex">
+            <p>{getValues('prefecture')}</p>
+            <p>{getValues('city')}</p>
+            <p>{getValues('address')}</p>
+            <p>{getValues('building')}</p>
+          </div>
+        </div>
         <div className="mt-8 flex flex-col items-center justify-center gap-2">
           <button
             type="submit"
