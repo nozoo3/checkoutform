9669a5d3d7d1b6a1171b758ff34254380cb20621
nozomi <dog1518rocky@gmail.com>
2025-02-24 19:53:38 +0900

    feat: Enhance CheckoutForm with validation for user inputs

--- src/components/features/CheckoutForm/index.tsx
+++ src/components/features/CheckoutForm/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { ConfirmModal } from './ConfirmModal';
-import { useForm, FormProvider } from 'react-hook-form';
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
 import { Label } from '~/components/ui/Label';
 import { TextInput } from '~/components/ui/TextInput';
 import { Heading1, Heading2 } from '~/components/ui/Heading';
@@ -35,7 +35,15 @@ export const CheckoutForm = () => {
   const methods = useForm<FormField>({
     mode: 'onSubmit',
   });
-  const { register } = methods;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const onSubmit: SubmitHandler<FormField> = () => {
+    setShowConfirmModal(true);
+  };
 
   return (
     <div className="p-8">
@@ -45,32 +53,67 @@ export const CheckoutForm = () => {
       </div>
       <FormProvider {...methods}>
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setShowConfirmModal(true);
-          }}
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="mt-4 mb-8 flex flex-col p-8 outline outline-gray-300">
             <div className="mb-4 flex flex-col gap-2">
               <Label label="氏名" required />
               <div className="flex gap-2">
-                <TextInput placeHolder="姓" register={register('firstName')} />
-                <TextInput placeHolder="名" register={register('lastName')} />
+                <TextInput
+                  placeHolder="姓"
+                  register={register('firstName', {
+                    required: '氏名を入力してください',
+                  })}
+                />
+                <TextInput
+                  placeHolder="名"
+                  register={register('lastName', {
+                    required: '氏名を入力してください',
+                  })}
+                />
               </div>
+              {(errors?.firstName?.message || errors?.lastName?.message) && (
+                <p className="text-xs text-red-500">
+                  {errors?.firstName?.message || errors?.lastName?.message}
+                </p>
+              )}
             </div>
             <div className="mb-4 flex max-w-1/3 flex-col gap-2">
               <Label label="電話番号" required />
               <TextInput
                 placeHolder="08012345678"
-                register={register('phone')}
+                register={register('phone', {
+                  required: '電話番号を入力してください',
+                  pattern: {
+                    value: /^[0-9]{11}$/,
+                    message: '電話番号は11桁の数字で入力してください',
+                  },
+                })}
               />
+              {errors?.phone?.message && (
+                <p className="text-xs text-red-500">{errors?.phone?.message}</p>
+              )}
             </div>
-            <div className="mb-4 flex max-w-1/6 flex-col gap-2">
+            <div className="mb-4 flex flex-col gap-2">
               <Label label="郵便番号" required />
-              <TextInput
-                placeHolder="0000000"
-                register={register('postCode')}
-              />
+              <div className="max-w-1/6">
+                <TextInput
+                  placeHolder="0000000"
+                  register={register('postCode', {
+                    required: '電話番号を入力してください',
+                    pattern: {
+                      value: /^[0-9]{7}$/,
+                      message: '郵便番号は7桁の数字で入力してください',
+                    },
+                  })}
+                />
+              </div>
+              {errors?.postCode?.message && (
+                <p className="text-xs text-red-500">
+                  {errors?.postCode?.message}
+                </p>
+              )}
             </div>
             <div className="mb-4 flex flex-col gap-2">
               <Label label="都道府県" required />
@@ -86,11 +129,27 @@ export const CheckoutForm = () => {
             </div>
             <div className="mb-4 flex flex-col gap-2">
               <Label label="市区町村" required />
-              <TextInput register={register('city')} />
+              <TextInput
+                register={register('city', {
+                  required: '市区町村を入力してください',
+                })}
+              />
+              {errors?.city?.message && (
+                <p className="text-xs text-red-500">{errors?.city?.message}</p>
+              )}
             </div>
             <div className="mb-4 flex flex-col gap-2">
               <Label label="番地" required />
-              <TextInput register={register('address')} />
+              <TextInput
+                register={register('address', {
+                  required: '番地を入力してください',
+                })}
+              />
+              {errors?.address?.message && (
+                <p className="text-xs text-red-500">
+                  {errors?.address?.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <Label label="建物名・部屋番号" />
